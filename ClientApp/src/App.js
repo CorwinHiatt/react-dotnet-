6ae2255902abb5e7c7242038a8ae9af6f2154a40
Corwin Hiatt <corwinhiatt@yahoo.com>
2022-11-14 16:32:21 -0500

    not much done

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { Alert } from 'reactstrap';
 import AppRoutes from './AppRoutes';
 import { Layout } from './components/Layout';
 import './custom.css';
@@ -10,6 +11,11 @@ export default class App extends Component {
   render() {
     return (
       <Layout>
+        
+        <h1>Its corwin</h1>
+        <button 
+>PUSH ME</button>
+        
         <Routes>
           {AppRoutes.map((route, index) => {
             const { element, ...rest } = route;
